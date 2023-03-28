ee58b813fc3e5a0ff8bc5bd2accb3898ad16657d
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-03-28 22:43:13 +0600

    customise code

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,33 +3,33 @@ import ParticularCard from '../components/ParticularCard';
 import { useProduct } from '../context/ProductProvider';
 
 const Cart = () => {
-    const {state: {products, error, loading, cart}} = useProduct()
-    console.log(products);
+  const { state: { products, error, loading, cart } } = useProduct()
+  console.log(products);
 
-    let content;
+  let content;
 
-    if (loading) {
-        content = <p>Loading</p>;
-      }
-    
-      if (error) {
-        content = <p>Something went wrong</p>;
-      }
-    
-      if (!loading && !error && cart.length === 0) {
-        content = <p>Nothing to show, product list is empty</p>;
-      }
-    
-      if (!loading && !error && cart.length) {
-        content = cart.map((product) => (
-          <ParticularCard key={product._id} product={product}></ParticularCard>
-        ));
-      }
+  if (loading) {
+    content = <p>Loading</p>;
+  }
+
+  if (error) {
+    content = <p>Something went wrong</p>;
+  }
+
+  if (!loading && !error && cart.length === 0) {
+    content = <p>Nothing to show, product list is empty</p>;
+  }
+
+  if (!loading && !error && cart.length) {
+    content = cart.map((product) => (
+      <ParticularCard key={product._id} product={product}></ParticularCard>
+    ));
+  }
 
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-        {content}
+      {content}
     </div>
   )
 };
