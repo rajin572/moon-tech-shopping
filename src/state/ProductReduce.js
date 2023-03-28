a97d694402e315eca64189f90426756e535f3854
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-03-28 22:10:58 +0600

    customise code

--- src/state/ProductReduce.js
+++ src/state/ProductReduce.js
@@ -1,52 +1,52 @@
 import { ActionTypes } from "./ActionType";
 
-export const InitialState =  {
-    loading: false,
-    products: [],
-    error: false,
-    cart: [],
-    wishlist: [],
+export const InitialState = {
+  loading: false,
+  products: [],
+  error: false,
+  cart: [],
+  wishlist: [],
 }
 
 export const ProductReduce = (state, action) => {
-    switch(action.type){
-        case ActionTypes.FETCHING_START:
-            return {
-              ...state,
-              loading: true,
-              error: false,
-            };
-          case ActionTypes.FETCHING_SUCCESS:
-            return {
-              ...state,
-              loading: false,
-              products: action.payload,
-              error: false,
-            };
-          case ActionTypes.FETCHING_ERROR:
-            return {
-              ...state,
-              loading: false,
-              error: true,
-            };
-          case ActionTypes.ADD_TO_CART:
-            return {
-              ...state,
-              cart: [...state.cart, action.payload],
-            };
-          case ActionTypes.ADD_TO_WISHLIST:
-            return {
-              ...state,
-              wishlist: [...state.wishlist, action.payload],
-            };
-          case ActionTypes.REMOVE_FROM_CART:
-            return {
-                ...state,
-                cart: state.cart.filter((item) => item !== action.payload)
-            };
-        default:
-        return state
-    };
-    
+  switch (action.type) {
+    case ActionTypes.FETCHING_START:
+      return {
+        ...state,
+        loading: true,
+        error: false,
+      };
+    case ActionTypes.FETCHING_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        products: action.payload,
+        error: false,
+      };
+    case ActionTypes.FETCHING_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
+    case ActionTypes.ADD_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, action.payload],
+      };
+    case ActionTypes.ADD_TO_WISHLIST:
+      return {
+        ...state,
+        wishlist: [...state.wishlist, action.payload],
+      };
+    case ActionTypes.REMOVE_FROM_CART:
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item !== action.payload)
+      };
+    default:
+      return state
+  };
+
 
 };
