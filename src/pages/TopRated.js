d93067a1f9b8668578e49641c9ce9070c6198bb4
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-03-28 23:02:56 +0600

    customise code

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -3,33 +3,33 @@ import ProductCard from '../components/ProductCard';
 import { useProduct } from '../context/ProductProvider';
 
 const TopRated = () => {
-    const {state: {products, error, loading}} = useProduct()
-    console.log(products);
+  const { state: { products, error, loading } } = useProduct()
+  console.log(products);
 
-    let content;
+  let content;
 
-    if (loading) {
-        content = <p>Loading</p>;
-      }
-    
-      if (error) {
-        content = <p>Something went wrong</p>;
-      }
-    
-      if (!loading && !error && products.length === 0) {
-        content = <p>Nothing to show, product list is empty</p>;
-      }
-    
-      if (!loading && !error && products.length) {
-        content = products.filter((product) => product.rating >= 5).map((product) => (
-          <ProductCard key={product._id} product={product}></ProductCard>
-        ));
-      }
+  if (loading) {
+    content = <p>Loading</p>;
+  }
+
+  if (error) {
+    content = <p>Something went wrong</p>;
+  }
+
+  if (!loading && !error && products.length === 0) {
+    content = <p>Nothing to show, product list is empty</p>;
+  }
+
+  if (!loading && !error && products.length) {
+    content = products.filter((product) => product.rating >= 5).map((product) => (
+      <ProductCard key={product._id} product={product}></ProductCard>
+    ));
+  }
 
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-        {content}
+      {content}
     </div>
   );
 };
